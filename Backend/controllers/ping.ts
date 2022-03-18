f1e9d96fc7f269ad529d41510b07fd5707835dfb
Peekoe <peekoecode@gmail.com>
2022-03-18 02:04:54 -0700

    Add docker files, add example post path, add prettier formatting

--- Backend/controllers/ping.ts
+++ Backend/controllers/ping.ts
@@ -1,15 +1,24 @@
-import { Get, Route } from "tsoa";
+import { Get, Post, Query, Route } from 'tsoa';
 
 interface PingResponse {
     message: string;
 }
 
-@Route("ping")
+@Route('ping')
 export default class PingController {
-    @Get("/")
+    @Get('/')
     public async getMessage(): Promise<PingResponse> {
         return {
-            message: "pong",
+            message: 'pong'
+        };
+    }
+
+    // check out tsoa docs for decorators
+    // https://tsoa-community.github.io/docs/getting-started.html
+    @Post('/')
+    public async postMessage(@Query() message?: string): Promise<PingResponse> {
+        return {
+            message: 'pong'
         };
     }
 }
