5cb23c361279c4b3372de0537775f4938da715b1
Daniel Truong <aduy1122@gmail.com>
2022-04-28 00:10:07 -0700

    Adding instruction + animation

--- Frontend/pages/index.tsx
+++ Frontend/pages/index.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from 'next'
 import {Banner} from './components/Banner'
+import Instruction from './components/Instruction'
 
 
 
 const Home: NextPage = () => {
   return (
-    <section className='flex my-12 mx-[80px] font-poppins'>
+    <section className='flex items-center flex-col my-12 mx-[80px] font-poppins'>
     <Banner/>
+    <Instruction/>
     </section>
   )
 }
